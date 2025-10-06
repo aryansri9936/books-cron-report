29c62f3e33beade9fe423ef194f548e9a6663ef5
aryansri9936 <143255078+aryansri9936@users.noreply.github.com>
2025-10-06 22:34:39 +0530

    Create books.js

---
+++ models/books.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  author: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  publishedYear: {
+    type: Number
+  },
+  genre: {
+    type: String,
+    trim: true
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Book', bookSchema);
